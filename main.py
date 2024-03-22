0366f9f793b7d28559bfc9249d0d818b66fca22d
Peter Husenbeth <peter.husenbeth@gmail.com>
2024-03-22 20:40:11 +0100

    rewrote get_estimators, added map_deviations

--- main.py
+++ main.py
@@ -3,19 +3,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-def get_estimators(df, independent_variable):
+def map_deviations(df_from: pd.DataFrame, df_to: pd.DataFrame, indep_var_col_index_from: int = 0, indep_var_col_index_to: int = 0):
+    # input x values from training data into estimators
+    # calculate deviation (Least-Square) of train x,y and ideal x,y
+    # get the lowest deviation and save the index no of the ideal function it came from
+
+    df_deviations = pd.DataFrame(columns=df_from.columns[1:].insert(0, 'func_nr'), dtype=np.float64)
+
+    # return None
+
+    # insert rows as lists of deviations
+    # for each estimator, calculate the sum of all y-deviations squared with every estimator
+    # x = df_from.iloc[:, indep_var_col_index_from].values.reshape(-1, 1)
+
+    func_nr = 0
+
+    for estimator in get_estimators(df_to, indep_var_col_index_to):
+
+        i = 0
+        func_nr += 1
+        row = {'func_nr': func_nr}
+
+        for column in df_from.columns:
+
+            if i != indep_var_col_index_from:
+                
+                row[column] = square_deviation(df_from[['x', column]], estimator)
+
+            i += 1
+
+        df_deviations.loc[len(df_deviations.index)] = row
+
+    return df_deviations
+
+def get_estimators(df: pd.DataFrame, indep_var_col_index: int = 0):
     # for every dependent column of the input DataFrame this function returns an estimator in a list
 
-    i = 0
     estimators = []
     lr = LinearRegression()
+    x = df.iloc[:, indep_var_col_index].values.reshape(-1, 1)
+    i = 0
 
-    for column_index in df.axes[1]:
-        if column_index == independent_variable:
+    for column in df.columns:
 
-            x = df.iloc[:, i].values.reshape(-1, 1)
-
-        else:
+        if i != indep_var_col_index:
 
             y = df.iloc[:, i].values.reshape(-1, 1)
 
@@ -24,30 +55,27 @@ def get_estimators(df, independent_variable):
         i += 1
 
     return estimators
-    
+
+def square_deviation(data: pd.DataFrame, function: LinearRegression):
+    #print("printing data: \n" + repr(data))
+    return 8.908
 
 def main():
     df_train = pd.read_csv(filepath_or_buffer="Datasets1/train.csv")
-    x_train = df_train.iloc[:, 0].values.reshape(-1, 1)
-    y1_itrain = df_train.iloc[:, 1].values.reshape(-1, 1)
-
-
     df_ideal = pd.read_csv(filepath_or_buffer="Datasets1/ideal.csv")
-    x_ideal = df_ideal.iloc[:, 0].values.reshape(-1, 1)
-    y1_ideal = df_ideal.iloc[:, 1].values.reshape(-1, 1)
 
-    # print(df_ideal.iloc[:, :])
-    # print(df_ideal.axes[1].__iter__)
 
-    # step 1
-    # get estimator for every of the 50 functions (--> array or list with all the estimators)
-    ideal_estimators = get_estimators(df_ideal, 'x')
+    map_dev = map_deviations(df_train, df_ideal)
 
-    # input x values from training data into estimators
+    print("map_deviations: ")
+    print(map_dev)
+    #print(get_estimators(df_ideal))
 
-    # calculate deviation (Least-Square) of train x,y and ideal x,y
+    #print(df_train.columns)
+    #print(df_train.columns[1:].insert(0, 'ideal_func_no'))
+    #print(["ideal_func_no"].append(df_train.columns[1:]))
 
-    # get the lowest deviation and save the index no of the ideal function it came from
+
 
 
 
