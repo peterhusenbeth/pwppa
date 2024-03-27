07a0b4ea64a8966045960662b6e327fc713b2145
Peter Husenbeth <peter.husenbeth@gmail.com>
2024-03-27 16:24:09 +0100

    rewrite for only one training function at a time.

--- main.py
+++ main.py
@@ -3,22 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-def map_deviations(df_from_x: pd.DataFrame, df_from_y: pd.DataFrame, df_to_x: pd.DataFrame, df_to_y: pd.DataFrame):
+def map_deviations(df_from_x: pd.DataFrame, df_from_y: pd.DataFrame, estimators: list[LinearRegression]):
     # input x values from training data into estimators
     # calculate deviation (Least-Square) of train x,y and ideal x,y
-    # get the lowest deviation and save the index no of the ideal function it came from
 
-    df_deviations = pd.DataFrame(columns=df_from_y.columns.insert(0, 'func_nr'), dtype=np.float64)
-
-    # return None
+    df_deviations = pd.DataFrame(columns=['func_nr', 'estimator', 'sqr_deviation', 'max_deviation'])
 
     # insert rows as lists of deviations
     # for each estimator, calculate the sum of all y-deviations squared with every estimator
-    # x = df_from.iloc[:, indep_var_col_index_from].values.reshape(-1, 1)
 
     func_nr = 0
-
-    estimators = get_estimators(df_to_x, df_to_y)
 
     # iterate 50 ideal functions
     for estimator in estimators:
@@ -26,14 +20,12 @@ def map_deviations(df_from_x: pd.DataFrame, df_from_y: pd.DataFrame, df_to_x: pd
         func_nr += 1
         row = {'func_nr': func_nr}
 
-        # iterate 4 training columns
-        for column in df_from_y.columns:
-                
-            row[column] = square_deviation(df_from_x, df_from_y[column], estimator)
+        row['estimator'] = estimator
+        row['sqr_deviation'], row['max_deviation'] = deviations(df_from_x, df_from_y, estimator)
 
         df_deviations.loc[len(df_deviations.index)] = row
 
-    return df_deviations, estimators
+    return df_deviations
 
 def get_estimators(df_x: pd.DataFrame, df_y: pd.DataFrame):
     # for every dependent column of the input DataFrame this function returns an estimator in a list
@@ -55,26 +47,43 @@ def get_estimators(df_x: pd.DataFrame, df_y: pd.DataFrame):
 
     return estimators
 
-def square_deviation(x_data: pd.Series, y_data: pd.Series, function: LinearRegression):
+def deviations(x_data: pd.Series, y_data: pd.Series, function: LinearRegression):
     # calculates the square deviation
     # (die Summe ist aber in Anhängigkeit davon, wie viele Datenpunkte zum Vergleich mit der Funktion genommen werden!)
 
     y_column = np.array(y_data).reshape(-1, 1)
     y_estimated = function.predict(np.array(x_data).reshape(-1, 1))
+    y_dev_abs = np.abs(y_column - y_estimated)
 
-    return np.float64(sum((y_column - y_estimated)**2))
+    # calculate square deviation
+    sqr_deviation = np.float64(sum((y_dev_abs)**2))
+
+    # find maximum single deviation
+    max_deviation = np.float64(y_dev_abs.max())
+
+    return sqr_deviation, max_deviation
 
 def main():
     # read CSV files into DataFrames
     df_train = pd.read_csv(filepath_or_buffer="pwppa/Datasets1/train.csv")
     df_ideal = pd.read_csv(filepath_or_buffer="pwppa/Datasets1/ideal.csv")
+    df_test = pd.read_csv(filepath_or_buffer="pwppa/Datasets1/test.csv")
 
+    # first step of the program
     # create table of square deviations from training data to ideal functions
-    map_dev, estimators = map_deviations(df_train.iloc[:, 0], df_train.iloc[:, 1:5], df_ideal.iloc[:, 0], df_ideal.iloc[:, 1:51])
+    estimators = get_estimators(df_ideal.iloc[:, 0], df_ideal.iloc[:, 1:51])
 
-    # print("map_deviations split: ")
-    # print(map_dev)
+    for column in df_train.columns:
 
+        if column != 'x':
+
+            map_dev = map_deviations(df_train['x'], df_train[column], estimators)
+
+            print("map_deviations split: ")
+            print(map_dev)
+
+
+    '''
     # sort each column ascending
     for column in map_dev.columns:
         if column != 'func_nr':
@@ -94,24 +103,27 @@ def main():
     plt.xlabel('y')
     plt.legend()
     plt.show()
+    '''
+
+    # second step of the program
+    # determine the largest deviation between training dataset and corresponding ideal function
+
+    # check every test datapoint
+    # for index, row in df_test.iterrows():
+        
+
+
+
+
+
+
+
+
 
     # OO Approach
     # class ideal function inherits from LinearRegression
     # class training data inherits from Dataframe?
     # class test data inherits from series?
-
-
-
-
-
-
-
-
-
-    # plot this shit
-    # plt.scatter(x_ideal, y1_ideal)
-    # plt.plot(x_ideal, y1_func, color='red')
-    # plt.show()
 
 if __name__ == '__main__':
     main()
